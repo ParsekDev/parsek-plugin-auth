5fd83c32a7f43a2a44220167b6c8e016b36390e2
kahverengi <ahmet@duruer.dev>
2026-02-01 01:33:24 +0300

    feat: made compatible with latest system property and mail plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,8 +37,8 @@ dependencies {
         compileOnly("dev.parsek:core:1.0.0-beta.19")
         compileOnly("dev.parsek:parsek-plugin-database:1.0.0-dev.5")
         compileOnly("dev.parsek:parsek-plugin-token:1.0.0-dev.8")
-        compileOnly("dev.parsek:parsek-plugin-mail:1.0.0-dev.4")
-        compileOnly("dev.parsek:parsek-plugin-system-property:1.0.0-dev.3")
+        compileOnly("dev.parsek:parsek-plugin-mail:1.0.0-dev.6")
+        compileOnly("dev.parsek:parsek-plugin-system-property:1.0.0-dev.4")
     }
 
     compileOnly(kotlin("stdlib-jdk8"))
