e6c5ed35b8f314e00b28c2a57e1e6de6c86f7e0a
kahverengi <ahmet@duruer.dev>
2026-02-01 01:23:07 +0300

    feat: made compatible with latest database plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     } else {
         compileOnly("dev.parsek:core:1.0.0-beta.19")
         compileOnly("dev.parsek:parsek-plugin-database:1.0.0-dev.5")
-        compileOnly("dev.parsek:parsek-plugin-token:1.0.0-dev.6")
+        compileOnly("dev.parsek:parsek-plugin-token:1.0.0-dev.8")
         compileOnly("dev.parsek:parsek-plugin-mail:1.0.0-dev.4")
         compileOnly("dev.parsek:parsek-plugin-system-property:1.0.0-dev.3")
     }
